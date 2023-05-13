e7995f75a28f07062974a08325a87d1d35a24d0e
FireC657 <comptagedepoints@gmail.com>
2023-05-13 16:35:58 +0100

    Integrated Hex mode

--- locksmith.c
+++ locksmith.c
@@ -5,6 +5,7 @@
 #include "util.h"
 
 bool_t encodeBase64=FALSE;
+bool_t encodeHexMod=FALSE;
 
 bool_t countPossibilites=FALSE;
 
@@ -20,7 +21,7 @@ int main(int argc, char** argv){
       //bool_t badOption = TRUE;
       if(!strcmp(argv[i],"--base64")){
          encodeBase64=TRUE;
-
+         encodeHexMod=FALSE;
          if(!(i+1 < argc)){
             printf("Please specify a length.\n");
             printf("\t%s %s ####\n",argv[0],argv[1]);
@@ -35,12 +36,32 @@ int main(int argc, char** argv){
             printf("Can\'t not accept negative or null lengths.\n");
             return 1;
          }
-
          if(!(i+2 < argc))break; 
          i+=2;
       }
 
-      if(!strcmp(argv[i],"--c-possibilites")){
+      if(!strcmp(argv[i],"--hex")){
+         encodeHexMod=TRUE;
+         encodeBase64=FALSE;
+         if(!(i+1 < argc)){
+            printf("Please specify a length.\n");
+            printf("\t%s %s ####\n",argv[0],argv[1]);
+            putchar('\t');
+            for(int i = 0; i < strlen(argv[0])+strlen(argv[1])+1;i++)putchar(' ');
+            printf(" ^~~~~~~~ Specify length here.\n");
+            return 1;
+         }
+         passlen = str2uint(argv[i+1]);
+
+         if(passlen == -1){
+            printf("Can\'t not accept negative or null lengths.\n");
+            return 1;
+         }
+         if(!(i+2 < argc))break; 
+         i+=2;
+      }
+     
+      if(!strcmp(argv[i],"--count-combinations")){
          countPossibilites=TRUE;
       }
    }
@@ -51,7 +72,20 @@ int main(int argc, char** argv){
       free(out_base64);
       
       if(countPossibilites){
-         long p = pow2(passlen,strlen(BASE64_TOKEN));
+         long p = pow2(passlen,(int)strlen(BASE64_TOKEN));
+         printf("There are %ld possibilites.\n",p);
+      }
+
+      return 0;
+   }
+
+   if(encodeHexMod){
+      char* out_base64 = hex_pass(passlen);
+      printf("%s\n",out_base64);
+      free(out_base64);
+      
+      if(countPossibilites){
+         long p = pow2(passlen,(int)strlen(HEXADEC_TOKEN));
          printf("There are %ld possibilites.\n",p);
       }
 
