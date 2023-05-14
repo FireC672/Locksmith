97eb0e6976c16694d1ecf58716f9455e85b24bb7
FireC657 <comptagedepoints@gmail.com>
2023-05-14 21:38:58 +0100

    Implemented 'in' in C

--- util.h
+++ util.h
@@ -2,11 +2,34 @@
 #define UTILITY_HPP
 
 #include <stdio.h>
+#include <string.h>
 
 #define __fatal_assertion__(msg) printf("\033[31m\033[1mfatal error: \033[0m%s",msg);\
                                  exit(1);
 #define __runtime_assertion__(msg) printf("\033[31m\033[1mruntime error: \033[0m%s",msg);\
                                  exit(1);
+
+#define IN_ARRAY(val, arr, len) ({ \
+    bool found = false; \
+    for (int i = 0; i < len; i++) { \
+        if (arr[i] == val) { \
+            found = true; \
+            break; \
+        } \
+    } \
+    found; \
+})
+
+#define INSTR_ARRAY(val, arr, len) ({ \
+    bool found = false; \
+    for (int i = 0; i < len; i++) { \
+        if (!strcmp(arr[i],val)) { \
+            found = true; \
+            break; \
+        } \
+    } \
+    found; \
+})
 
 void printout_help();
 
